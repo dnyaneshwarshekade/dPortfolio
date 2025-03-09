b4b5a8b2fa6e03f68b3a54cc15fe88f33a1003d4
dipesh <dipnangle@gmail.com>
2025-03-09 14:10:44 +0530

    home page changes for css class common and short

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
     return (
         <>
             <div className='home'>
-                <div className='lg:w-1/2 md:w-1/2 flex justify-center px-10 lg:pt-0 pt-40'>
-                    <div className='w-full h-full'>
+                <div className='homeDiv1'>
+                    <div className='whf'>
                         <h1 className='title'>
                             Hey There 👋 <br />
                         </h1>
@@ -70,38 +70,38 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div className='lg:w-1/2 md:w-1/2 lg:m-0 md:m-0 sm:flex sm:flex-col sm:items-center'>
-                    <div className="pt-20 w-full lg:p-0 md:p-0 px-4">
+                <div className='homeDiv2'>
+                    <div className='codeSection'>
                         {/* Code Block (VS Code Style) */}
-                        <div className='bg-gray-100 dark:bg-[#1e1e2e] text-gray-900 dark:text-white rounded-lg p-4 shadow-lg dark:shadow-lg dark:shadow-black/20 w-full max-w-[460px] border border-gray-300 dark:border-gray-700'>
+                        <div className='codingBlock'>
                             {/* Top Bar (VS Code Style) */}
-                            <div className="flex items-center space-x-2 pb-2 border-b border-gray-300 dark:border-gray-600">
-                                <div className="w-3 h-3 bg-red-500 rounded-full"></div>
-                                <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
-                                <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+                            <div className='codingBlockTitle'>
+                                <div className='w-3 h-3 bg-red-500 rounded-full'></div>
+                                <div className='w-3 h-3 bg-yellow-500 rounded-full'></div>
+                                <div className='w-3 h-3 bg-green-500 rounded-full'></div>
                             </div>
 
                             {/* Code Content */}
-                            <pre className="lg:text-sm md:text-sm text-xs font-mono mt-2">
-                                <code className="text-gray-600 dark:text-green-400">// My Profile Code Block</code>
+                            <pre className='codingBlockBody'>
+                                <code className='codingComment'>// My Profile Code Block</code>
                                 <br />
-                                <code className="text-blue-600 dark:text-blue-400">const</code> <code className="text-pink-500 dark:text-pink-400">me</code> <code className="text-gray-900 dark:text-white">=</code> <code className="text-yellow-600 dark:text-yellow-400">{`{`}</code>
+                                <code className='text-blue-600 dark:text-blue-400'>const</code> <code className='text-pink-500 dark:text-pink-400'>me</code> <code className='text-gray-900 dark:text-white'>=</code> <code className='text-yellow-600 dark:text-yellow-400'>{`{`}</code>
                                 <br />
-                                <code className="ml-8 text-gray-800 dark:text-white">name:</code> <code className="text-yellow-500 dark:text-yellow-300">"Dipesh Nangle"</code>,
+                                <code className='ml-8 text-gray-800 dark:text-white'>name:</code> <code className='text-yellow-500 dark:text-yellow-300'>'Dipesh Nangle'</code>,
                                 <br />
-                                <code className="ml-8 text-gray-800 dark:text-white">role:</code> <code className="text-yellow-500 dark:text-yellow-300">"Software Engineer"</code>,
+                                <code className='ml-8 text-gray-800 dark:text-white'>role:</code> <code className='text-yellow-500 dark:text-yellow-300'>'Software Engineer'</code>,
                                 <br />
-                                <code className="ml-8 text-gray-800 dark:text-white">skills:</code> <code className="text-yellow-500 dark:text-yellow-300 text-wrap">["Python", "React", "Tailwind", <Link to="/skills">"More..."</Link>]</code>,
+                                <code className='ml-8 text-gray-800 dark:text-white'>skills:</code> <code className='text-yellow-500 dark:text-yellow-300 text-wrap'>['Python', 'React', 'Tailwind', <Link to='/skills'>'More...'</Link>]</code>,
                                 <br />
-                                <code className="text-yellow-600 dark:text-yellow-400">{`}`}</code>;
+                                <code className='text-yellow-600 dark:text-yellow-400'>{`}`}</code>;
                             </pre>
                         </div>
                     </div>
-                    <div className='w-full flex justify-end content-end mt-6'>
-                        <img className='w-[380px] lg:ml-auto md:ml-auto lg:m-0 lg:p-0' src={boyCoding} alt="Boy Coding" />
+                    <div className='boySection'>
+                        <img className='boyCoding' src={boyCoding} alt="Boy Coding" />
                     </div>
                 </div>
-                <div className='flex justify-center py-10 lg:hidden md:hidden'>
+                <div className='contactUs'>
                     <ContactUs />
                 </div>
             </div>
